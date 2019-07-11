618a89a19b21c1c802d7888a94e5390139c72978
shiyutim <shiyutim@outlook.com>
2019-07-11 15:49:51 +0800

    update

---
+++ Vue项目2/src/api/ajax.js
@@ -0,0 +1,37 @@
+/*
+
+*/
+import axios from 'axios'
+export default function ajax(url, data={}, type='GET') {
+
+
+
+    return new Promise(function(resolve, reject) {
+
+        let promise 
+        if(type === 'GET') {
+            let dataStr = ''
+            Object.keys(data).forEach(key => {
+                dataStr += key + '=' + data[key] + '&'
+            })
+            if(dataStr !== '') {
+                dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+                url = url + '?' + dataStr
+            }
+            promise = axios.get(url)
+        }else {
+            promise = axios.post(url, data)
+        }
+        promise.then(function(response) {
+            resolve(response.data)
+        }).catch(function(error) {
+            reject(error)
+        })
+        //执行异步Ajax请求
+        //成功了调用resolve()
+        //失败了调用reject()
+    })
+
+   
+
+}
---
+++ Vue项目2/src/pages/Own/OwnOne.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>This is my title</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    
+</style>
